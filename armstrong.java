e7074f25e80a83c0731d671701277f88598097bd
Swaroop442 <saiswaroop406@gmail.com>
2024-12-30 17:20:02 +0530

    message

--- armstrong.java
+++ armstrong.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class amstrong {
+public class armstrong {
 	public static void main(String[] args) {
 		System.out.println("Enter a Number :");
 		Scanner sc = new Scanner(System.in);
